e107b252f8e8ebe0b8ff12eba9a61a5865e4f330
anastasiialosytska <anastasiia.losytska@rubygarage.org>
2023-11-22 15:47:44 +0100

    Fix rubocop errors

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -13,10 +13,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url
-    ActionController::Base.helpers.asset_path('fallback/default_photo.png')
-  end
-
   def extension_allowlist
     %w[jpg jpeg gif png]
   end
---
+++ spec/uploaders/photo_uploader_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe PhotoUploader do
+  include CarrierWave::Test::Matchers
+
+  let(:real_estate_object) { build(:real_estate_object) }
+  let(:path) { Rails.root.join('app/assets/images/default_photo.png') }
+  let(:uploader) { described_class.new(real_estate_object, :photo) }
+
+  after do
+    described_class.enable_processing = false
+    uploader.remove!
+  end
+
+  context 'when stores real estate object photo avatar' do
+    before do
+      described_class.enable_processing = true
+      File.open(path) { |f| uploader.store!(f) }
+    end
+
+    it 'scales down a photo to be exactly 200 by 200 pixels' do
+      expect(uploader.thumb).to have_dimensions(200, 200)
+    end
+  end
+end
